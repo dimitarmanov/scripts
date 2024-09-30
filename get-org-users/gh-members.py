fe4eab2033a8734eb4e1ac27979f53da0606fa2f
Dimitar Manov <99065541+dimitarmanov@users.noreply.github.com>
2024-09-30 20:47:53 +0300

    members of certain team and subteam

--- get-org-users/gh-members.py
+++ get-org-users/gh-members.py
@@ -11,20 +11,34 @@ load_dotenv()
 
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 GITHUB_ORG = os.getenv('GITHUB_ORG')
+TARGET_TEAM_SLUG = 'customers'
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
+# Initialize GitHub API
 api = GhApi(token=GITHUB_TOKEN, owner=GITHUB_ORG)
 
-teams = api.teams.list(GITHUB_ORG)
+# Set to store unique members
+all_members = set()
 
-team_slugs = [team['slug'] for team in teams]
 
-for team in team_slugs:
-    members = api.teams.list_members_in_org(GITHUB_ORG, team)
-
+# Recursive function to fetch members from a team and its subteams
+def get_team_members_and_subteams(org, team_slug):
+    print(team_slug)
+    # Get members of the current team
+    members = api.teams.list_members_in_org(org, team_slug)
     for member in members:
         all_members.add(member['login'])
 
+    # Get subteams of the current team
+    subteams = api.teams.list_child_in_org(org, team_slug)
+    for subteam in subteams:
+        get_team_members_and_subteams(org, subteam['slug'])
+
+
+# Fetch the main team and its subteams
+get_team_members_and_subteams(GITHUB_ORG, TARGET_TEAM_SLUG)
+
+# Convert the set to a list and print unique members
 unique_members = list(all_members)
 print(unique_members)
